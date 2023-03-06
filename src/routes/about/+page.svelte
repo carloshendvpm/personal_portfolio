c3e8944b1af668dd42038a46c6e26ff84a4608bd
Carlos Henrique <hencarlosdv@gmail.com>
2023-03-06 09:57:25 -0300

    adicionando timeline a aba sobre

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,35 @@
-<h1>Sobre</h1>
+<script>
+	import {
+		Timeline,
+		TimelineItem,
+		TimelineSeparator,
+		TimelineDot,
+		TimelineConnector,
+		TimelineContent,
+		TimelineOppositeContent
+	} from 'svelte-vertical-timeline';
+
+	const options = [
+		{ time: '2018 - 2020', title: 'Téc em Desenvolvimento de Sistemas - ETE PORTO DIGITAL' },
+		{ title: 'Analise e Desenvolvimento de Sistemas - SENAC', time: '2021 - 2023' },
+		{ title: 'Estagiário em Desenvolvimento Front-end - Secretária de Defesa Social PE', time: 'FEV 2022 - OUT 2022' },
+		{ time: 'OUT 2022 - momento', title: 'Desenvolvedor em Real Seguro Viagem' }
+	];
+</script>
+
+<Timeline position="alternate">
+	{#each options as option}
+		<TimelineItem>
+			<TimelineOppositeContent slot="opposite-content">
+				<p>{option.time}</p>
+			</TimelineOppositeContent>
+			<TimelineSeparator>
+				<TimelineDot />
+				<TimelineConnector />
+			</TimelineSeparator>
+			<TimelineContent>
+				<h3>{option.title}</h3>
+			</TimelineContent>
+		</TimelineItem>
+	{/each}
+</Timeline>
